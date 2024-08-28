f09d846ea90d54f8c773fb2e72f53fd9636722ac
andrademariaandrade <165902324+andrademariaandrade@users.noreply.github.com>
2024-08-28 07:58:20 -0300

    logica javasrict

---
+++ index.js
@@ -0,0 +1,32 @@
+// Usando var
+var nome = "john;"
+
+// Usando let
+let idade = 25;
+
+// Usando const
+const pi = 3.14;
+
+let a = 5;
+let b = 10;
+let soma = a + b; // Armazena o resultado da soma de a e b na variável sum console.log(soma); // 15
+
+ let a = 5;
+ let b = 10;
+
+ function multiplica (x, y) {
+    return x * y;
+ }
+
+ let produto = multiplica(a, b); // Armazena o resultado da função multiplica na variável produto
+ console.log(produto) ; // 50
+
+ let frutas = ['Apple' , 'Banana', 'Cherry'];
+ console.log(frutas[1]); // Banana
+
+ let pessoa = {
+    primeironome: 'john',
+    sobrenome: 'Delon',
+    age: 25
+ };
+ console.log(pessoa.primeironome); // John
